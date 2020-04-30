06fe2e5e3307a435cb1dab86bba0768f79c48849
balazagi <ssadri@vld353.cmpd1.metoffice.gov.uk>
2020-04-30 08:46:47 +0100

    Reverting to "Merge pull request #19 from MetOffice/delete_ascend" as that is the last good known state
    This reverts commit 0f024bddbdb4a855af6b6c7cef801b1630cf6f5c, reversing
    changes made to b97fb3aecbfa8a975234955f3e126f58e560f296.

--- lib/catnip/preparation.py
+++ lib/catnip/preparation.py
@@ -8,6 +8,7 @@ import iris.analysis
 import numpy as np
 from six import string_types, integer_types
 import iris.coord_categorisation as iccat
+from ascend import shape
 import os.path
 import config
 
@@ -326,6 +327,52 @@ def add_time_coord_cats(cube):
         print(('add_time_coord_cats: {}, skipping . . . '.format(err)))
     except ValueError as err:
         print(('add_time_coord_cats: {}, skipping . . . '.format(err)))
+
+
+def extract_rot_cube(cube, min_lat, min_lon, max_lat, max_lon):
+    """
+    Function etracts the specific region from the cube.
+
+    args
+    ----
+    cube: cube on rotated coord system, used as reference grid for transformation.
+
+    Returns
+    -------
+    min_lat: The minimum latitude point of the desired extracted cube.
+    min_lon: The minimum longitude point of the desired extracted cube.
+    max_lat: The maximum latitude point of the desired extracted cube.
+    max_lon: The maximum longitude point of the desired extracted cube.
+
+    An example:
+
+    >>> file = os.path.join(config.DATA_DIR, 'rcm_monthly.pp')
+    >>> cube = iris.load_cube(file, 'air_temperature')
+    >>> min_lat = 50
+    >>> min_lon = -10
+    >>> max_lat = 60
+    >>> max_lon = 0
+    >>> extracted_cube = extract_rot_cube(cube, min_lat, min_lon, max_lat, max_lon)
+    >>> print(np.max(extracted_cube.coord('latitude').points))
+    61.47165097005264
+    >>> print(np.min(extracted_cube.coord('latitude').points))
+    48.213032844268646
+    >>> print(np.max(extracted_cube.coord('longitude').points))
+    3.642576550089792
+    >>> print(np.min(extracted_cube.coord('longitude').points))
+    -16.385571344717235
+    """
+    # adding unrotated coords to the cube
+    add_aux_unrotated_coords(cube)
+
+    # use ASCEND package to cut the area out
+    corners = [(min_lon, min_lat), (min_lon, max_lat),
+               (max_lon, max_lat), (max_lon, min_lat)]
+    rectangle = shape.create(corners, {'shape': 'rectangle'}, 'Polygon')
+    extracted_cube = rectangle.extract_subcube(cube)
+
+
+    return extracted_cube
 
 
 def remove_forecast_coordinates(iris_cube):
