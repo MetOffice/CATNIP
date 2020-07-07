970da0d3714cffe0f08a8cd5c438f64432277623
Nick Savage <nhsavage42@gmail.com>
2020-07-07 17:04:56 +0100

    make changes to implement tests previously on incorrect branch #42

--- lib/catnip/tests/test_utils.py
+++ lib/catnip/tests/test_utils.py
@@ -19,10 +19,15 @@ class TestUtils(unittest.TestCase):
         file4 = os.path.join(conf.DATA_DIR, 'daily.19990808_19990830.pp')
 
         self.ua_cube = iris.load_cube(file1)
-        self.gcm_t_cube = iris.load(file2,'air_temperature')
-        self.gcm_u_cube = iris.load(file2,'x_wind')
+        self.gcm_t_cube = iris.load_cube(file2,'air_temperature')
+        self.gcm_u_cube = iris.load_cube(file2,'x_wind')
         self.daily_01_08_cube = iris.load_cube(file3)
         self.daily_08_30_cube = iris.load_cube(file4)
+
+        # a cube with no time coordinate
+        self.daily_01_08_cube_notimedim = self.daily_01_08_cube.copy()
+        time_coord = self.daily_01_08_cube_notimedim.coord('time')
+        self.daily_01_08_cube_notimedim.remove_coord(time_coord)
 
     @classmethod
     def tearDownClass(cls):
@@ -43,12 +48,10 @@ class TestUtils(unittest.TestCase):
         self.assertEqual(c1.shape,c2.shape)
 
         # passing a cube with no time coordinate
-        daily_01_08_cube_notimedim = self.daily_01_08_cube.copy()
-        time_coord = daily_01_08_cube_notimedim.coord('time')
-        daily_01_08_cube_notimedim.remove_coord(time_coord)
-
-        self.assertRaises(KeyError, common_timeperiod, daily_01_08_cube_notimedim, self.daily_08_30_cube)
-        self.assertRaises(KeyError, common_timeperiod, self.daily_08_30_cube, daily_01_08_cube_notimedim)
+        self.assertRaises(KeyError, common_timeperiod,
+                          self.daily_01_08_cube_notimedim, self.daily_08_30_cube)
+        self.assertRaises(KeyError, common_timeperiod,
+                          self.daily_08_30_cube, self.daily_01_08_cube_notimedim)
 
         # passing a cube with no time bound
         daily_01_08_cube_nobound = self.daily_01_08_cube.copy()
@@ -122,10 +125,38 @@ class TestUtils(unittest.TestCase):
     def test_get_date_range(self):
         """Tests for get_date_range function"""
 
+        """Tests for get_date_range function"""
+
         # First file - ua
         start_str, end_str, dr_constraint = get_date_range(self.ua_cube)
         self.assertEqual(start_str, '1/11/490')
         self.assertEqual(end_str, '1/12/747')
+        self.assertIsInstance(dr_constraint, iris.Constraint)
+
+        # File 2 x_wind from GCM
+        start_str, end_str, dr_constraint = get_date_range(self.gcm_u_cube)
+        self.assertEqual(start_str, '1/9/1960')
+        self.assertEqual(end_str, '1/10/1960')
+        self.assertIsInstance(dr_constraint, iris.Constraint)
+
+        # File 3
+        start_str, end_str, dr_constraint = get_date_range(self.daily_01_08_cube)
+        self.assertEqual(start_str, '1/8/1999')
+        self.assertEqual(end_str, '23/8/1999')
+        self.assertIsInstance(dr_constraint, iris.Constraint)
+
+        # File 4
+        start_str, end_str, dr_constraint = get_date_range(self.daily_08_30_cube)
+        self.assertEqual(start_str, '8/8/1999')
+        self.assertEqual(end_str, '30/8/1999')
+        self.assertIsInstance(dr_constraint, iris.Constraint)
+
+        # pass a cube with no time dimension
+        self.assertRaises(iris.exceptions.CoordinateNotFoundError,
+                          get_date_range, self.daily_01_08_cube_notimedim)
+
+        # Pass an integer
+        self.assertRaises(AttributeError, get_date_range, 1)
 
 
     def test_sort_cube(self):
