83aa26f441398b3cf0a2638dbf9d62f02516645b
zmaalick <zubair.maalick@metoffice.gov.uk>
2020-08-18 16:39:53 +0100

    update: bash script with suggested changes #115

--- bin/run_all_tests.sh
+++ bin/run_all_tests.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-set -eu
 
 module load scitools
 
@@ -15,55 +14,56 @@ export LC_CTYPE=en_GB.utf8
 FILES_TO_TEST=$(find $CATNIP_LIB_DIR -type f -name '*.py')
 
 
-
-function test_flake8 {
-	echo -e "\033[1;32m[RUNNING FLAKE8 TESTS]\033[0m"
+function check_flake8 {
+	echo -e "\033[1;32mRunning flake8 tests ...\033[0m"
 	flake8
 }
 
-function test_black {
-	echo -e "\033[1;32m[RUNNING BLACK TESTS]\033[0m"
+function check_black {
+	echo -e "\033[1;32mRunning black tests ...\033[0m"
 	module unload scitools
 	module load scitools/experimental-current
-        black $FILES_TO_TEST	
+        black $FILES_TO_TEST
 
 }
 
-function test_documentation {
-	echo -e "\033[1;32m[RUNNING DOCUMENTATION TESTS]\033[0m"
+function check_documentation {
+	echo -e "\033[1;32mChecking documentation ...\033[0m"
 	echo "TODO"
 }
 
-function test_license {
-        echo -e "\033[1;32m[RUNNING DOCUMENTATION TESTS]\033[0m"
+function check_license {
+        echo -e "\033[1;32mChecking for license ...\033[0m"
         echo "TODO"
 }
 
-function test_pytest {
-	echo -e "\033[1;32m[RUNNING PY TESTS]\033[0m"
-	cd $CATNIP_LIB_DIR	
+function check_pytest {
+	echo -e "\033[1;32mRunning pytest ...\033[0m"
+	cd $CATNIP_LIB_DIR
 	pytest -p no:warnings -v
 }
 
 
-# ----------------------------------
-# tests starts here 
-# ---------------------------------
-
-# Run flake8 tests
-test_flake8
-
-# Run black codestyling tests
-test_black
-
-# Run black codestyling tests
-test_documentation
-
-# Run black codestyling tests
-test_documentation
-
-# Run doc and unittests 
-test_pytest
+# initializing the dict for summary
+declare -A results
 
 
+# test runs here ...
 
+TESTS="flake8 black documentation license pytest"
+for TEST_NAME in $TESTS; do
+    "check_$TEST_NAME"
+    ret=$?
+		if [ $ret = "0" ]; then
+			results["$TEST_NAME"]="\033[1;32m[PASS]\033[0m"
+		else
+			results["$TEST_NAME"]="\033[1;31m[FAIL]\033[0m"
+		fi
+
+done
+
+# printing the summary
+echo -e "\n\033[1;34m[TEST SUIT COMPLETE]\033[0m"
+for key in ${!results[@]}; do
+    echo -e ${key} ' check ' ${results[${key}]}
+done
